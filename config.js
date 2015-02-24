7157cf1ef168a68daad87a1b62a6666bc6887c92
zachasme <zachasme@gmail.com>
2015-02-24 19:27:44 +0100

    Add stuff

--- config.js
+++ config.js
@@ -464,4 +464,3 @@ System.config({
     }
   }
 });
-
--- lib/actor/components/actor_list_item.jsx
+++ lib/actor/components/actor_list_item.jsx
@@ -11,7 +11,7 @@ export const ActorListItem = React.createClass({
   render() {
     return (
       <div>
-        <p><Anchor href={"#actors/" + this.props.actor.get('id') }>{ this.props.actor.get('navn') }</Anchor> ({ this.props.actor.get('type') })</p>
+        <p><Anchor href={"actors/" + this.props.actor.get('id') }>{ this.props.actor.get('navn') }</Anchor> ({ this.props.actor.get('type') })</p>
       </div>
     )
   },
