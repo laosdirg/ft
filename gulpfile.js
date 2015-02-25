76b8fb99fa22daccbc4471d9678054ce50b3b752
zachasme <zachasme@gmail.com>
2015-02-25 14:24:12 +0100

    Add build watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,30 @@
-import gulp from 'gulp'
+var gulp = require('gulp')
+var path = require('path')
+var Builder = require('systemjs-builder')
+var watch = require('gulp-watch');
+var livereload = require('gulp-livereload');
 
-gulp.task('default', () => {
-  console.log('lol')
+gulp.task('default', ['build'], function (cb) {
+  livereload.listen()
+
+  watch(['lib/**/*.js', 'lib/**/*.jsx'], function(){
+    gulp.start('build');
+  })
+
+});
+
+gulp.task('build', function(){
+  var builder = new Builder()
+
+  return builder
+    .loadConfig('./config.js')
+    .then(function(){
+      return builder.build('bin/browser', 'dist/outfile.js')
+        .then(function() {
+          console.log('Build complete');
+          livereload.reload('dist/outfile.js');
+        })
+    }).catch(function(err){
+      console.log('Build error', err);
+    })
 })
