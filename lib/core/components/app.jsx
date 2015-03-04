08707fc70ebfb233448ab1a7a811b418c7e2bfb2
zachasme <zachasme@gmail.com>
2015-03-04 16:58:22 +0100

    Make paths absolute

--- lib/core/components/app.jsx
+++ lib/core/components/app.jsx
@@ -12,9 +12,9 @@ export const FTApp = React.createClass({
       <div className="FTApp">
         <nav className="App-nav">
           <h1><Anchor href="/">FT</Anchor></h1>
-          <Anchor href="actors">Aktører</Anchor>
-          <Anchor href="cases">Sager</Anchor>
-          <Anchor href="derp">Blindgyde</Anchor>
+          <Anchor href="/actors">Aktører</Anchor>
+          <Anchor href="/cases">Sager</Anchor>
+          <Anchor href="/derp">Blindgyde</Anchor>
           <SearchContainer />
         </nav>
         <main>
